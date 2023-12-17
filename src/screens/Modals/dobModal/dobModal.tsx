c768a0496f31626f8c065d64427890a41285ef22
Adil Allahverdiyev <Adil.Allahverdiyev@axapppuk.onmicrosoft.com>
2023-12-17 14:49:32 +0000

    🐛 dob wrong date fix

--- src/screens/Modals/dobModal/dobModal.tsx
+++ src/screens/Modals/dobModal/dobModal.tsx
@@ -125,10 +125,11 @@ export const DobModal = () => {
         monthsData.filter(item => item.month === selectedMonth)[0],
       ),
       selectedDay,
+      12,
     );
     const splitter = dob.toUTCString().split(', ')[1].split(' ');
     const convertedDob = `${splitter[0]}-${splitter[1]}-${splitter[2]}`;
-    console.log(convertedDob);
+    console.log(splitter, dob);
     dispatch(updateUserDetails({ dob: convertedDob }));
     dispatch(closeBottomSheet());
   };
